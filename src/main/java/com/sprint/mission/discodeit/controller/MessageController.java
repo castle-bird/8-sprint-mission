d63a3de1594bb4888e9971ddbbb06dbabd43b184
castle-bird <seongjo0501@gmail.com>
2026-01-04 20:39:53 +0900

    feat: Swagger - ReadStatusController 수정, MessageController 수정

--- src/main/java/com/sprint/mission/discodeit/controller/MessageController.java
+++ src/main/java/com/sprint/mission/discodeit/controller/MessageController.java
@@ -4,7 +4,15 @@ import com.sprint.mission.discodeit.dto.request.BinaryContentCreateRequest;
 import com.sprint.mission.discodeit.dto.request.MessageCreateRequest;
 import com.sprint.mission.discodeit.dto.request.MessageUpdateRequest;
 import com.sprint.mission.discodeit.entity.Message;
+import com.sprint.mission.discodeit.entity.ReadStatus;
 import com.sprint.mission.discodeit.service.MessageService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -19,6 +27,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+@Tag(name = "Message Controller", description = "Message 관련 Controller입니다.")
 @RequiredArgsConstructor
 @Controller
 @ResponseBody
@@ -27,55 +36,56 @@ public class MessageController {
 
   private final MessageService messageService;
 
+  @Operation(summary = "메세지 생성", description = "메세지를 생성하는 컨트롤러 입니다.")
+  @ApiResponses(value = {
+      @ApiResponse(responseCode = "201", description = "생성 성공", content = @Content(schema = @Schema(implementation = Message.class))),
+      @ApiResponse(responseCode = "500", description = "서버 오류", content = @Content(schema = @Schema(hidden = true)))})
   @PostMapping(path = "create", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
   public ResponseEntity<Message> create(
       @RequestPart("messageCreateRequest") MessageCreateRequest messageCreateRequest,
-      @RequestPart(value = "attachments", required = false) List<MultipartFile> attachments
-  ) {
+      @RequestPart(value = "attachments", required = false) List<MultipartFile> attachments) {
     List<BinaryContentCreateRequest> attachmentRequests = Optional.ofNullable(attachments)
-        .map(files -> files.stream()
-            .map(file -> {
-              try {
-                return new BinaryContentCreateRequest(
-                    file.getOriginalFilename(),
-                    file.getContentType(),
-                    file.getBytes()
-                );
-              } catch (IOException e) {
-                throw new RuntimeException(e);
-              }
-            })
-            .toList())
-        .orElse(new ArrayList<>());
+        .map(files -> files.stream().map(file -> {
+          try {
+            return new BinaryContentCreateRequest(file.getOriginalFilename(), file.getContentType(),
+                file.getBytes());
+          } catch (IOException e) {
+            throw new RuntimeException(e);
+          }
+        }).toList()).orElse(new ArrayList<>());
     Message createdMessage = messageService.create(messageCreateRequest, attachmentRequests);
-    return ResponseEntity
-        .status(HttpStatus.CREATED)
-        .body(createdMessage);
+    return ResponseEntity.status(HttpStatus.CREATED).body(createdMessage);
   }
 
+  @Operation(summary = "메세지 수정", description = "메세지를 수정하는 컨트롤러 입니다.")
+  @ApiResponses(value = {
+      @ApiResponse(responseCode = "200", description = "수정 성공", content = @Content(schema = @Schema(implementation = Message.class))),
+      @ApiResponse(responseCode = "500", description = "서버 오류", content = @Content(schema = @Schema(hidden = true)))})
   @PatchMapping("/update")
   public ResponseEntity<Message> update(@RequestParam("messageId") UUID messageId,
       @RequestBody MessageUpdateRequest request) {
     Message updatedMessage = messageService.update(messageId, request);
-    return ResponseEntity
-        .status(HttpStatus.OK)
-        .body(updatedMessage);
+    return ResponseEntity.status(HttpStatus.OK).body(updatedMessage);
   }
 
+  @Operation(summary = "메세지 제거", description = "메세지를 제거하는 컨트롤러 입니다.")
+  @ApiResponses(value = {
+      @ApiResponse(responseCode = "200", description = "제거 성공", content = @Content(schema = @Schema(hidden = true))),
+      @ApiResponse(responseCode = "500", description = "서버 오류", content = @Content(schema = @Schema(hidden = true)))})
   @DeleteMapping("/delete")
   public ResponseEntity<Void> delete(@RequestParam("messageId") UUID messageId) {
     messageService.delete(messageId);
-    return ResponseEntity
-        .status(HttpStatus.NO_CONTENT)
-        .build();
+    return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
   }
 
+  @Operation(summary = "메세지 조회", description = "각채널의 메세지를 조회하는 컨트롤러 입니다.")
+  @ApiResponses(value = {
+      @ApiResponse(responseCode = "200", description = "수정 성공", content = @Content(array = @ArraySchema(schema = @Schema(implementation = Message.class)))),
+      @ApiResponse(responseCode = "500", description = "서버 오류", content = @Content(schema = @Schema(hidden = true)))})
   @GetMapping("/findAllByChannelId")
   public ResponseEntity<List<Message>> findAllByChannelId(
       @RequestParam("channelId") UUID channelId) {
     List<Message> messages = messageService.findAllByChannelId(channelId);
-    return ResponseEntity
-        .status(HttpStatus.OK)
-        .body(messages);
+    return ResponseEntity.status(HttpStatus.OK).body(messages);
   }
 }
--- src/main/java/com/sprint/mission/discodeit/controller/ReadStatusController.java
+++ src/main/java/com/sprint/mission/discodeit/controller/ReadStatusController.java
@@ -26,7 +26,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import java.util.List;
 import java.util.UUID;
 
-@Tag(name = "ReadStatus Controller", description = "User의 상태관련 Controller입니다.")
+@Tag(name = "ReadStatus Controller", description = "User의 각체널 메세지 읽은 시간을 저장하는 Controller입니다.")
 @RequiredArgsConstructor
 @Controller
 @ResponseBody
@@ -49,7 +49,7 @@ public class ReadStatusController {
 
   @Operation(summary = "사용자의 메세지 읽음 상태 수정", description = "사용자가 채팅방의 글을 마지막으로 읽은 시간을 업데이트하는 컨트롤러입니다.")
   @ApiResponses(value = {
-      @ApiResponse(responseCode = "200", description = "생성 성공", content = @Content(schema = @Schema(implementation = ReadStatus.class))),
+      @ApiResponse(responseCode = "200", description = "수정 성공", content = @Content(schema = @Schema(implementation = ReadStatus.class))),
       @ApiResponse(responseCode = "500", description = "서버 오류", content = @Content(schema = @Schema(hidden = true)))})
   @PostMapping("/update")
   public ResponseEntity<ReadStatus> update(@RequestParam("readStatusId") UUID readStatusId,
@@ -62,7 +62,7 @@ public class ReadStatusController {
 
   @Operation(summary = "특정 사용자가 접속 가능한 채널 모두 찾기", description = "특정 사용자가 메세지 읽음 상태를 이용해 입장 가능한 채널의 목록을 나타냅니다.")
   @ApiResponses(value = {
-      @ApiResponse(responseCode = "200", description = "생성 성공", content = @Content(array = @ArraySchema(schema = @Schema(implementation = ReadStatus.class)))),
+      @ApiResponse(responseCode = "200", description = "조회 성공", content = @Content(array = @ArraySchema(schema = @Schema(implementation = ReadStatus.class)))),
       @ApiResponse(responseCode = "500", description = "서버 오류", content = @Content(schema = @Schema(hidden = true)))})
   @GetMapping("/findAllByUserId")
   public ResponseEntity<List<ReadStatus>> findAllByUserId(@RequestParam("userId") UUID userId) {
