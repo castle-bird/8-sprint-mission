3d1c3a8a26d753640fddd9af8fd9a20e514aaa78
castle-bird <seongjo0501@gmail.com>
2026-01-04 20:34:46 +0900

    feat: Swagger - ReadStatusController 수정

--- src/main/java/com/sprint/mission/discodeit/controller/ReadStatusController.java
+++ src/main/java/com/sprint/mission/discodeit/controller/ReadStatusController.java
@@ -3,7 +3,15 @@ package com.sprint.mission.discodeit.controller;
 import com.sprint.mission.discodeit.dto.request.ReadStatusCreateRequest;
 import com.sprint.mission.discodeit.dto.request.ReadStatusUpdateRequest;
 import com.sprint.mission.discodeit.entity.ReadStatus;
+import com.sprint.mission.discodeit.entity.User;
 import com.sprint.mission.discodeit.service.ReadStatusService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,6 +26,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import java.util.List;
 import java.util.UUID;
 
+@Tag(name = "ReadStatus Controller", description = "User의 상태관련 Controller입니다.")
 @RequiredArgsConstructor
 @Controller
 @ResponseBody
@@ -26,6 +35,10 @@ public class ReadStatusController {
 
   private final ReadStatusService readStatusService;
 
+  @Operation(summary = "사용자의 메세지 읽음 상태 생성", description = "사용자가 채팅방의 글을 마지막으로 읽은 시간을 저장하는 컨트롤러입니다.")
+  @ApiResponses(value = {
+      @ApiResponse(responseCode = "201", description = "생성 성공", content = @Content(schema = @Schema(implementation = ReadStatus.class))),
+      @ApiResponse(responseCode = "500", description = "서버 오류", content = @Content(schema = @Schema(hidden = true)))})
   @PostMapping("/create")
   public ResponseEntity<ReadStatus> create(@RequestBody ReadStatusCreateRequest request) {
     ReadStatus createdReadStatus = readStatusService.create(request);
@@ -34,6 +47,10 @@ public class ReadStatusController {
         .body(createdReadStatus);
   }
 
+  @Operation(summary = "사용자의 메세지 읽음 상태 수정", description = "사용자가 채팅방의 글을 마지막으로 읽은 시간을 업데이트하는 컨트롤러입니다.")
+  @ApiResponses(value = {
+      @ApiResponse(responseCode = "200", description = "생성 성공", content = @Content(schema = @Schema(implementation = ReadStatus.class))),
+      @ApiResponse(responseCode = "500", description = "서버 오류", content = @Content(schema = @Schema(hidden = true)))})
   @PostMapping("/update")
   public ResponseEntity<ReadStatus> update(@RequestParam("readStatusId") UUID readStatusId,
       @RequestBody ReadStatusUpdateRequest request) {
@@ -43,6 +60,10 @@ public class ReadStatusController {
         .body(updatedReadStatus);
   }
 
+  @Operation(summary = "특정 사용자가 접속 가능한 채널 모두 찾기", description = "특정 사용자가 메세지 읽음 상태를 이용해 입장 가능한 채널의 목록을 나타냅니다.")
+  @ApiResponses(value = {
+      @ApiResponse(responseCode = "200", description = "생성 성공", content = @Content(array = @ArraySchema(schema = @Schema(implementation = ReadStatus.class)))),
+      @ApiResponse(responseCode = "500", description = "서버 오류", content = @Content(schema = @Schema(hidden = true)))})
   @GetMapping("/findAllByUserId")
   public ResponseEntity<List<ReadStatus>> findAllByUserId(@RequestParam("userId") UUID userId) {
     List<ReadStatus> readStatuses = readStatusService.findAllByUserId(userId);
