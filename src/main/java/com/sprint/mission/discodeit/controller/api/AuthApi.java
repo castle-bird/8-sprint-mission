8c459a6d892be60ce89b0bdc492c491b0e943c5e
castle-bird <seongjo0501@gmail.com>
2026-01-12 18:19:17 +0900

    feat: 로그인 swagger 수정

--- src/main/java/com/sprint/mission/discodeit/controller/api/AuthApi.java
+++ src/main/java/com/sprint/mission/discodeit/controller/api/AuthApi.java
@@ -5,6 +5,7 @@ import com.sprint.mission.discodeit.entity.User;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -20,5 +21,16 @@ public interface AuthApi {
       @ApiResponse(responseCode = "200", description = "로그인 성공", content = @Content(schema = @Schema(implementation = User.class))),
       @ApiResponse(responseCode = "401", description = "인증 실패 (아이디/비밀번호 불일치)", content = @Content(schema = @Schema(hidden = true))),
       @ApiResponse(responseCode = "500", description = "서버 오류", content = @Content(schema = @Schema(hidden = true)))})
-  ResponseEntity<User> login(LoginRequest loginRequest);
+  ResponseEntity<User> login(
+      @RequestBody(
+          description = "로그인 정보",
+          required = true,
+          content = @Content(
+              mediaType = "application/json",
+              schema = @Schema(
+                  implementation = LoginRequest.class
+              )
+          )
+      )
+      LoginRequest loginRequest);
 }
