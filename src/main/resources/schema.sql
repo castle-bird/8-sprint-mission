08fe01756c2f6539b32a4e7352b9f1b3f0319c1a
castle-bird <seongjo0501@gmail.com>
2026-01-15 16:37:35 +0900

    feat: JPA 세팅

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -9,8 +9,8 @@ DROP TYPE IF EXISTS channel_type CASCADE;
 
 CREATE TABLE IF NOT EXISTS binary_contents
 (
-    id           uuid PRIMARY KEY      DEFAULT gen_random_uuid(),
-    created_at   timestamptz  NOT NULL DEFAULT NOW(),
+    id           uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+    created_at   timestamptz  NOT NULL,
     file_name    VARCHAR(255) NOT NULL,
     size         BIGINT       NOT NULL,
     content_type VARCHAR(100) NOT NULL,
@@ -19,8 +19,8 @@ CREATE TABLE IF NOT EXISTS binary_contents
 
 CREATE TABLE IF NOT EXISTS users
 (
-    id         uuid PRIMARY KEY             DEFAULT gen_random_uuid(),
-    created_at timestamptz         NOT NULL DEFAULT NOW(),
+    id         uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+    created_at timestamptz         NOT NULL,
     updated_at timestamptz,
     username   VARCHAR(50) UNIQUE  NOT NULL,
     email      VARCHAR(100) UNIQUE NOT NULL,
@@ -29,15 +29,13 @@ CREATE TABLE IF NOT EXISTS users
     CONSTRAINT fk_profile FOREIGN KEY (profile_id) REFERENCES binary_contents (id) ON DELETE SET NULL
 );
 
-
-
 CREATE TABLE IF NOT EXISTS user_status
 (
-    id             uuid PRIMARY KEY     DEFAULT gen_random_uuid(),
-    created_at     timestamptz NOT NULL DEFAULT NOW(),
+    id             uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+    created_at     timestamptz NOT NULL,
     updated_at     timestamptz,
-    user_id        uuid        NOT NULL,
-    last_active_at timestamptz NOT NULL DEFAULT NOW(),
+    user_id        uuid        NOT NULL UNIQUE,
+    last_active_at timestamptz NOT NULL,
     CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 );
 
@@ -45,18 +43,18 @@ CREATE TABLE IF NOT EXISTS user_status
 CREATE TYPE channel_type AS ENUM ('PUBLIC', 'PRIVATE');
 CREATE TABLE IF NOT EXISTS channels
 (
-    id          uuid PRIMARY KEY     DEFAULT gen_random_uuid(),
-    created_at  timestamptz NOT NULL DEFAULT NOW(),
+    id          uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+    created_at  timestamptz  NOT NULL,
     updated_at  timestamptz,
     name        VARCHAR(100),
     description VARCHAR(500),
-    type        channel_type
+    type        channel_type NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS messages
 (
-    id         uuid PRIMARY KEY     DEFAULT gen_random_uuid(),
-    created_at timestamptz NOT NULL DEFAULT NOW(),
+    id         uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+    created_at timestamptz NOT NULL,
     updated_at timestamptz,
     content    TEXT,
     channel_id uuid        NOT NULL,
@@ -67,14 +65,14 @@ CREATE TABLE IF NOT EXISTS messages
 
 CREATE TABLE IF NOT EXISTS read_status
 (
-    id           uuid PRIMARY KEY     DEFAULT gen_random_uuid(),
-    created_at   timestamptz NOT NULL DEFAULT NOW(),
+    id           uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+    created_at   timestamptz NOT NULL,
     updated_at   timestamptz,
     user_id      uuid        NOT NULL,
     channel_id   uuid        NOT NULL,
+    last_read_at timestamptz NOT NULL,
     CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE,
-    CONSTRAINT fk_channel FOREIGN KEY (channel_id) REFERENCES channels (id) ON DELETE CASCADE,
-    last_read_at timestamptz NOT NULL DEFAULT NOW()
+    CONSTRAINT fk_channel FOREIGN KEY (channel_id) REFERENCES channels (id) ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS message_attachments
