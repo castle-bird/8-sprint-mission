8a1a28120f23f0e9a4200f4123e54ab9b2af20d6
castle-bird <seongjo0501@gmail.com>
2026-02-03 15:08:12 +0900

    refactor: schema.sql 변경
    - H2데이터베이스 호환을 위해 timestamptz → timestamp with time zone

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,20 +1,20 @@
 CREATE TABLE IF NOT EXISTS binary_contents
 (
     id           uuid PRIMARY KEY,
-    created_at   timestamptz  NOT NULL,
-    file_name    VARCHAR(255) NOT NULL,
-    size         BIGINT       NOT NULL,
-    content_type VARCHAR(100) NOT NULL
+    created_at   timestamp with time zone NOT NULL,
+    file_name    VARCHAR(255)             NOT NULL,
+    size         BIGINT                   NOT NULL,
+    content_type VARCHAR(100)             NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS users
 (
     id         uuid PRIMARY KEY,
-    created_at timestamptz         NOT NULL,
-    updated_at timestamptz,
-    username   VARCHAR(50) UNIQUE  NOT NULL,
-    email      VARCHAR(100) UNIQUE NOT NULL,
-    password   VARCHAR(60)         NOT NULL,
+    created_at timestamp with time zone NOT NULL,
+    updated_at timestamp with time zone,
+    username   VARCHAR(50) UNIQUE       NOT NULL,
+    email      VARCHAR(100) UNIQUE      NOT NULL,
+    password   VARCHAR(60)              NOT NULL,
     profile_id uuid,
     CONSTRAINT fk_profile FOREIGN KEY (profile_id) REFERENCES binary_contents (id) ON DELETE SET NULL
 );
@@ -22,10 +22,10 @@ CREATE TABLE IF NOT EXISTS users
 CREATE TABLE IF NOT EXISTS user_status
 (
     id             uuid PRIMARY KEY,
-    created_at     timestamptz NOT NULL,
-    updated_at     timestamptz,
-    user_id        uuid        NOT NULL UNIQUE,
-    last_active_at timestamptz NOT NULL,
+    created_at     timestamp with time zone NOT NULL,
+    updated_at     timestamp with time zone,
+    user_id        uuid                     NOT NULL UNIQUE,
+    last_active_at timestamp with time zone NOT NULL,
     CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 );
 
@@ -34,20 +34,20 @@ CREATE TYPE channel_type AS ENUM ('PUBLIC', 'PRIVATE');
 CREATE TABLE IF NOT EXISTS channels
 (
     id          uuid PRIMARY KEY,
-    created_at  timestamptz  NOT NULL,
-    updated_at  timestamptz,
+    created_at  timestamp with time zone NOT NULL,
+    updated_at  timestamp with time zone,
     name        VARCHAR(100),
     description VARCHAR(500),
-    type        channel_type NOT NULL
+    type        channel_type             NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS messages
 (
     id         uuid PRIMARY KEY,
-    created_at timestamptz NOT NULL,
-    updated_at timestamptz,
+    created_at timestamp with time zone NOT NULL,
+    updated_at timestamp with time zone,
     content    TEXT,
-    channel_id uuid        NOT NULL,
+    channel_id uuid                     NOT NULL,
     author_id  uuid,
     CONSTRAINT fk_channel FOREIGN KEY (channel_id) REFERENCES channels (id) ON DELETE CASCADE,
     CONSTRAINT fk_user FOREIGN KEY (author_id) REFERENCES users (id) ON DELETE SET NULL
@@ -56,11 +56,11 @@ CREATE TABLE IF NOT EXISTS messages
 CREATE TABLE IF NOT EXISTS read_status
 (
     id           uuid PRIMARY KEY,
-    created_at   timestamptz NOT NULL,
-    updated_at   timestamptz,
-    user_id      uuid        NOT NULL,
-    channel_id   uuid        NOT NULL,
-    last_read_at timestamptz NOT NULL,
+    created_at   timestamp with time zone NOT NULL,
+    updated_at   timestamp with time zone,
+    user_id      uuid                     NOT NULL,
+    channel_id   uuid                     NOT NULL,
+    last_read_at timestamp with time zone NOT NULL,
     CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE,
     CONSTRAINT fk_channel FOREIGN KEY (channel_id) REFERENCES channels (id) ON DELETE CASCADE
 );
