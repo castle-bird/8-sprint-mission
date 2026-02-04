560779ae4ef36e4a994e316345f299424fe52482
castle-bird <seongjo0501@gmail.com>
2026-02-04 17:14:12 +0900

    feat: 유저 생성 테스트 리팩토링 및 추가

--- src/test/java/com/sprint/mission/discodeit/user/UserServiceTest.java
+++ src/test/java/com/sprint/mission/discodeit/user/UserServiceTest.java
@@ -1,17 +1,16 @@
 package com.sprint.mission.discodeit.user;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.then;
-import static org.mockito.Mockito.times;
 
 import com.sprint.mission.discodeit.dto.data.BinaryContentDto;
 import com.sprint.mission.discodeit.dto.data.UserDto;
 import com.sprint.mission.discodeit.dto.request.BinaryContentCreateRequest;
 import com.sprint.mission.discodeit.dto.request.UserCreateRequest;
+import com.sprint.mission.discodeit.dto.request.UserUpdateRequest;
 import com.sprint.mission.discodeit.entity.BinaryContent;
 import com.sprint.mission.discodeit.entity.User;
 import com.sprint.mission.discodeit.mapper.UserMapper;
@@ -22,7 +21,9 @@ import com.sprint.mission.discodeit.service.impl.BasicUserService;
 import com.sprint.mission.discodeit.storage.BinaryContentStorage;
 import java.util.Optional;
 import java.util.UUID;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -33,7 +34,6 @@ import org.mockito.junit.jupiter.MockitoExtension;
 @DisplayName("UserService 단위 테스트")
 public class UserServiceTest {
 
-  // 테스트 대상이 사용하는 의존성들 Mock으로 넣어줘야함 - 필수
   @Mock
   private UserRepository userRepository;
   @Mock
@@ -45,100 +45,176 @@ public class UserServiceTest {
   @Mock
   private BinaryContentStorage binaryContentStorage;
 
-  // 테스트 대상 - 위에 Mock들을 넣어줌
   @InjectMocks
   private BasicUserService userService;
 
-  @Test
-  @DisplayName("성공: 프로필이 없어도 유저가 생성된다.")
-  void create_user_without_profile() {
-    /**
-     * Given
-     */
-    UserCreateRequest request = new UserCreateRequest("Rex", "rex@gmail.com", "12345678");
-    UserDto expectedDto = new UserDto(UUID.randomUUID(), "Rex", "rex@gmail.com", null, true);
 
-    /* Service단 테스트에서 Repository의 영향을 배제하기 위해 아래처럼 가정 */
-    // 이메일 중복 없음을 가정
-    given(
-        userRepository.existsByEmail(anyString())
-    ).willReturn(false);
+  @Nested
+  @DisplayName("유저 생성 테스트")
+  class user_create {
 
-    // 이름 중복 없음을 가정
-    given(
-        userRepository.existsByUsername(anyString())
-    ).willReturn(false);
+    private UserCreateRequest userCreateRequest;
 
-    // 매퍼가 DTO 반환하도록 가정
-    given(
-        userMapper.toDto(any())
-    ).willReturn(expectedDto);
+    @BeforeEach
+    void setUp() {
+      userCreateRequest = new UserCreateRequest("Rex", "rex@gmail.com", "12345678");
 
-    /**
-     * When
-     */
-    UserDto result = userService.create(request, Optional.empty());
+      // 중복 Given
+      // 생성할때 이메일, 이름 중복검사 시행 했는지 체크
+      given(userRepository.existsByEmail(anyString())).willReturn(false);
+      given(userRepository.existsByUsername(anyString())).willReturn(false);
+    }
 
-    /**
-     * Then
-     */
-    assertThat(result).isNotNull();
-    assertEquals(result.username(), request.username());
-    assertEquals(result.email(), request.email());
+    @Test
+    @DisplayName("성공: 프로필이 없어도 유저가 생성된다.")
+    void create_user_without_profile() {
+      // Given
+      UserDto expectedDto = new UserDto(
+          UUID.randomUUID(),
+          "Rex",
+          "rex@gmail.com",
+          null,
+          true
+      );
+      given(
+          userMapper.toDto(any())
+      ).willReturn(expectedDto);
 
-    // 실제로 Repository가 호출 됐는지?
-    then(userRepository).should(times(1)).existsByEmail(request.email());
-    then(userRepository).should(times(1)).existsByUsername(request.username());
-    then(userRepository).should(times(1)).save(any(User.class));
+      // When
+      UserDto result = userService.create(userCreateRequest, Optional.empty());
+
+      // Then
+      then(userRepository).should().save(any(User.class));
+    }
+
+    @Test
+    @DisplayName("성공: 프로필 이미지를 포함하여 유저가 생성된다.")
+    void create_user_with_profile() {
+      // Given
+      BinaryContentCreateRequest profileRequest = new BinaryContentCreateRequest(
+          "rex.png",
+          "image/png",
+          "rex-image".getBytes()
+      );
+
+      BinaryContentDto binaryContentDto = new BinaryContentDto(
+          UUID.randomUUID(),
+          "rex.png",
+          (long) "rex-image".getBytes().length,
+          "image/png"
+      );
+
+      UserDto expectedDto = new UserDto(
+          UUID.randomUUID(),
+          "Rex",
+          "rex@gmail.com",
+          binaryContentDto,
+          true
+      );
+
+      given(
+          userMapper.toDto(any())
+      ).willReturn(expectedDto);
+
+      // When
+      UserDto result = userService.create(userCreateRequest, Optional.of(profileRequest));
+
+      // Then
+      then(userRepository).should().save(any(User.class));
+      then(binaryContentRepository).should().save(any(BinaryContent.class));
+      then(binaryContentStorage).should().put(any(), any());
+    }
   }
 
-  @Test
-  @DisplayName("성공: 프로필 이미지를 포함하여 유저가 생성된다.")
-  void create_user_with_profile() {
-    /**
-     * Given
-     */
-    UserCreateRequest request = new UserCreateRequest("Rex", "rex@gmail.com", "12345678");
-    BinaryContentCreateRequest profileRequest = new BinaryContentCreateRequest(
-        "rex.png", "image/png", "rex-image".getBytes()
-    );
-    BinaryContentDto binaryContentDto = new BinaryContentDto(UUID.randomUUID(), "rex.png",
-        (long) "rex-image".getBytes().length, "image/png");
-    UserDto expectedDto = new UserDto(UUID.randomUUID(), "Rex", "rex@gmail.com", binaryContentDto,
-        true);
+  // ================================================================================================
 
-    // 이메일 중복 없음을 가정
-    given(
-        userRepository.existsByEmail(anyString())
-    ).willReturn(false);
+  @Nested
+  @DisplayName("유저 수정 테스트")
+  class user_update {
 
-    // 이름 중복 없음을 가정
-    given(
-        userRepository.existsByUsername(anyString())
-    ).willReturn(false);
+    private User findUser;
+    private UserUpdateRequest userUpdateRequest;
 
-    // 매퍼가 DTO 반환하도록 가정
-    given(
-        userMapper.toDto(any())
-    ).willReturn(expectedDto);
+    @BeforeEach
+    void setUp() {
+      findUser = new User("Rex", "rex@gmail.com", "12345678", null);
+      userUpdateRequest = new UserUpdateRequest("Rex-수정", "rex@gmail.com-수정", "12345678-수정");
 
-    /**
-     * When
-     */
-    UserDto result = userService.create(request, Optional.of(profileRequest));
+      // 중복 Given
+      // 생성할때 아이디, 이메일, 이름 중복검사 시행 했는지 체크
+      given(
+          userRepository.findById(any())
+      ).willReturn(
+          Optional.of(findUser)
+      );
+      given(userRepository.existsByEmail(anyString())).willReturn(false);
+      given(userRepository.existsByUsername(anyString())).willReturn(false);
+    }
 
-    assertThat(result).isNotNull();
-    assertEquals(result.username(), request.username());
-    assertEquals(result.email(), request.email());
+    @Test
+    @DisplayName("성공: 프로필 없이도 유저 수정이 된다.")
+    void update_user_without_profile() {
+      // Given
+      UserDto expectedDto = new UserDto(
+          UUID.randomUUID(),
+          "Rex-수정",
+          "rex@gmail.com-수정",
+          null,
+          true
+      );
 
-    /**
-     * Then
-     */
-    // 실제로 Repository가 호출 됐는지?
-    then(userRepository).should(times(1)).existsByEmail(request.email());
-    then(userRepository).should(times(1)).existsByUsername(request.username());
-    then(userRepository).should(times(1)).save(any(User.class));
-    then(binaryContentRepository).should(times(1)).save(any(BinaryContent.class));
-    then(binaryContentStorage).should(times(1)).put(any(), any());
+      given(
+          userMapper.toDto(any())
+      ).willReturn(expectedDto);
+
+      // When
+      UserDto result = userService.update(
+          UUID.randomUUID(),
+          userUpdateRequest,
+          Optional.empty()
+      );
+
+      // Then
+      // 기존 유저와 수정 유저가 이름이 같은지?
+      assertEquals(expectedDto.username(), findUser.getUsername());
+    }
+
+    @Test
+    @DisplayName("성공: 프로필 있어도 유저 수정이 된다.")
+    void update_user_with_profile() {
+      // Given
+      BinaryContentCreateRequest profileRequest = new BinaryContentCreateRequest(
+          "rex.png",
+          "image/png",
+          "rex-image".getBytes()
+      );
+
+      BinaryContentDto binaryContentDto = new BinaryContentDto(
+          UUID.randomUUID(),
+          "rex.png",
+          (long) "rex-image".getBytes().length,
+          "image/png"
+      );
+
+      UserDto expectedDto = new UserDto(
+          UUID.randomUUID(),
+          "Rex-수정",
+          "rex@gmail.com-수정",
+          binaryContentDto,
+          true
+      );
+
+      // When
+      UserDto result = userService.update(
+          UUID.randomUUID(),
+          userUpdateRequest,
+          Optional.of(profileRequest)
+      );
+
+      // Then
+      then(binaryContentRepository).should().save(any(BinaryContent.class));
+      then(binaryContentStorage).should().put(any(), any());
+      assertEquals(expectedDto.username(), findUser.getUsername());
+    }
   }
 }
