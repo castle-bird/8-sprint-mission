a56c39fba54f9f308575a69ff9fc9ffb8a5fe4f7
castle-bird <seongjo0501@gmail.com>
2026-02-19 13:44:43 +0900

    refactor: schema.sql에 index 테이블명 수정

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -129,6 +129,6 @@ ALTER TABLE read_statuses
 -- 메시지 조회 (채널별, 시간순)
 CREATE INDEX idx_messages_channel_created ON messages (channel_id, created_at DESC);
 -- 읽기 상태 조회 (사용자+채널)
-CREATE INDEX idx_read_status_user_channel ON read_status (user_id, channel_id);
+CREATE INDEX idx_read_status_user_channel ON read_statuses (user_id, channel_id);
 -- 첨부파일 조회
 CREATE INDEX idx_message_attachments_message ON message_attachments (message_id);
